515d5d72d730f48376a854450820228a4acf4ddd
italorodrigues1 <italosport110@hotmail.com>
2026-03-15 20:14:54 -0300

    feat: remove default API URL and add auto increment to EAS build profiles

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:3333';
+export const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export const COLORS = {
   primary: '#169670',
